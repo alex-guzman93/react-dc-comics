cf15d9c7f73ea670474c4aef2609a461ed084b50
alexander guzman <Alex.networking93@gmail.com>
2026-01-20 00:46:39 +0100

    add exercise dinamically comics to  main,  no style

--- src/components/MainDc.jsx
+++ src/components/MainDc.jsx
@@ -1,10 +1,23 @@
 import './MainDc.css';
-
+import cards from '../data/comics';
 function MainDc() {
     return (
         <main>
+            {/* <h1>{'--> Content goes here <--'}</h1> */}
 
-            <h1>{'--> Content goes here <--'}</h1>
+            <ul>
+                {
+                    cards.map(card => {
+                        return (
+                            <li key={card.id} >
+                                <img src={card.thumb} alt={card.description} />
+                                <h3>{card.title}</h3>
+                            </li>
+                        )
+                    })
+                }
+
+            </ul>
 
         </main>
     )
